6ec3f919991367abb01b053ae9764b75b61ab857
rogeriocassares <roger10cassares@gmail.com>
2024-12-19 13:09:45 -0300

    update ci 0.3.5

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,6 @@ async function bootstrap() {
   app.use(cookieParser());
   app.enableCors(corsOptions);
 
-  await app.listen(smartcampusmauaServerPort, '0.0.0.0');
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
